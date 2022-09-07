524a8f059aadca3c60c699ee211841e85f082b72
crossbat <xowls1999@gmail.com>
2022-09-07 15:16:18 +0900

    Initial commit

---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.test
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+
+    }
+    private fun rollDice(){
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = (1..6).random()
+        resultText.text = randomInt.toString()
+    }
+}
